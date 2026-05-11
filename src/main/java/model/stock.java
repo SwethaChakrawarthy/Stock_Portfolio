9ce9cbdc33238e2d3348df13d817a33e7585a344
SwethaChakrawarthy <spothura@kent.edu>
2026-05-11 15:25:55 -0400

    Added Stock model with validation of price

---
+++ src/main/java/model/stock.java
@@ -0,0 +1,41 @@
+package model;
+
+public class Stock {
+    private String name;
+    private String symbol;
+    private double price;
+    private String sector;
+
+    public Stock(String name, String symbol, double price, String sector) {
+        this.name = name;
+        this.symbol = symbol;
+        if (price<=0){
+            throw new IllegalArgumentException("Price must be greater than zero");
+        }
+        this.price = price;
+        this.sector = sector;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setPrice(double price) {
+        if (price<=0){
+            throw new IllegalArgumentException("Price must be greater than 0");
+        }
+        this.price = price;
+    }
+}
